fc54a1228989eae3c6009cc36f469b3792d8cb37
Vitalii <avalor1991@gmail.com>
2018-10-02 22:06:57 -0400

    Added a Procfile.

--- server.js
+++ server.js
@@ -29,8 +29,7 @@ app.use(express.static("public"));
 // Connect to the Mongo DB
 // mongoose.connect("mongodb://localhost/nyPost2018", { useNewUrlParser: true });
 
-var MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nyPost2018";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
